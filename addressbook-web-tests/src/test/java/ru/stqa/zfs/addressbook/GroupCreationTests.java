1afd5fd4dd10221be54748e40d5cb0ca597c9361
zarinasim <zazylka@mail.ru>
2021-02-23 16:04:19 +0300

    Изменение фикстуры инициализации

--- addressbook-web-tests/src/test/java/ru/stqa/zfs/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/zfs/addressbook/GroupCreationTests.java
@@ -16,10 +16,6 @@ public class GroupCreationTests {
   public void setUp() throws Exception {
     wb = new FirefoxDriver();
     wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wb.get("http://localhost/addressbook/group.php");
     wb.findElement(By.name("user")).click();
     wb.findElement(By.name("user")).clear();
@@ -28,6 +24,10 @@ public class GroupCreationTests {
     wb.findElement(By.name("pass")).clear();
     wb.findElement(By.name("pass")).sendKeys("secret");
     wb.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
     wb.findElement(By.name("new")).click();
     wb.findElement(By.name("group_name")).click();
     wb.findElement(By.name("group_name")).clear();
