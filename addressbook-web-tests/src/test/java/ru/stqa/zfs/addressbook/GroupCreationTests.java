a592937b2d4eae5738539bbc72f9dfd02ae07844
zarinasim <zazylka@mail.ru>
2021-02-23 15:49:44 +0300

    Автотест

---
+++ addressbook-web-tests/src/test/java/ru/stqa/zfs/addressbook/GroupCreationTests.java
@@ -0,0 +1,69 @@
+package ru.stqa.zfs.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+public class GroupCreationTests {
+  private WebDriver wb;
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    wb = new FirefoxDriver();
+    wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
+    wb.get("http://localhost/addressbook/group.php");
+    wb.findElement(By.name("user")).click();
+    wb.findElement(By.name("user")).clear();
+    wb.findElement(By.name("user")).sendKeys("admin");
+    wb.findElement(By.name("pass")).click();
+    wb.findElement(By.name("pass")).clear();
+    wb.findElement(By.name("pass")).sendKeys("secret");
+    wb.findElement(By.xpath("//input[@value='Login']")).click();
+    wb.findElement(By.name("new")).click();
+    wb.findElement(By.name("group_name")).click();
+    wb.findElement(By.name("group_name")).clear();
+    wb.findElement(By.name("group_name")).sendKeys("test11");
+    wb.findElement(By.name("group_header")).click();
+    wb.findElement(By.name("group_header")).clear();
+    wb.findElement(By.name("group_header")).sendKeys("test11");
+    wb.findElement(By.name("group_footer")).click();
+    wb.findElement(By.name("group_footer")).clear();
+    wb.findElement(By.name("group_footer")).sendKeys("test12");
+    wb.findElement(By.name("submit")).click();
+    wb.findElement(By.xpath("//div[@id='content']/div")).click();
+    wb.findElement(By.linkText("group page")).click();
+    wb.findElement(By.linkText("Logout")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wb.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wb.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wb.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+}
