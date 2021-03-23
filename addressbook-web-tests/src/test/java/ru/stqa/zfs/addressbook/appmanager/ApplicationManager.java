703fc4b3c8644f0fdcc536267795ff0b7b3fa241
zarinasim <zazylka@mail.ru>
2021-03-23 17:21:12 +0300

    Задание 7

--- addressbook-web-tests/src/test/java/ru/stqa/zfs/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/zfs/addressbook/appmanager/ApplicationManager.java
@@ -1,14 +1,16 @@
 package ru.stqa.zfs.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
+  private final String browser;
   protected WebDriver wd;
 
   private SessionHelper sessionHelper;
@@ -16,8 +18,20 @@ public class ApplicationManager {
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
 
+  public ApplicationManager (String browser) {
+    this.browser = browser;
+  }
+
   public void init() {
-    wd = new FirefoxDriver();
+    String browser = BrowserType.CHROME;
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else  if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
+
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
@@ -31,7 +45,7 @@ public class ApplicationManager {
   public void stop() {
     wd.quit();
   }
-
+/*
   private boolean isElementPresent(By by) {
     try {
       wd.findElement(by);
@@ -39,7 +53,7 @@ public class ApplicationManager {
     } catch (NoSuchElementException e) {
       return false;
     }
-  }
+  }*/
 
   public GroupHelper getGroupHelper() {
     return groupHelper;
--- sandbox/src/main/java/ru/stqa/zfs/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/zfs/sandbox/Equation.java
@@ -13,19 +13,32 @@ public class Equation {
     this.b = b;
     this.c = c;
 
-    double d = b*b - 4*a*c;
-    if (d>0) {
-      n = 2;
+    double d = b * b - 4 * a * c;
+
+    if (a == 0) {
+      if (b == 0) {
+        if (c == 0) {
+          n = -1;
+        } else {
+          n = 0;
+        }
+      } else {
+        n = 1;
+      }
+
     } else {
-      if (d==0){
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
         n = 1;
       } else {
         n = 0;
       }
     }
+
   }
 
-  public int rootNumber(){
+  public int rootNumber() {
     return n;
   }
 
