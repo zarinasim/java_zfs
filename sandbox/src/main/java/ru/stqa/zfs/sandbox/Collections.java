2d015b09c4447b7d15d697880274db8544d69336
zarinasim <zazylka@mail.ru>
2021-03-24 17:02:27 +0300

    Лекция 4.2

---
+++ sandbox/src/main/java/ru/stqa/zfs/sandbox/Collections.java
@@ -0,0 +1,16 @@
+package ru.stqa.zfs.sandbox;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+  public static void main (String[] args){
+    String [] langs = { "Java", "C#","Python","PHP"};
+
+    List<String> languages = Arrays.asList("Java", "C#","Python","PHP");
+
+    for (String l : languages) {
+      System.out.println("Я хочу выучить " + l);
+    }
+  }
+}
